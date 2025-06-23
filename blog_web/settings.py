a6351840d25ba30ec18f87cb3f4f61ae427f601a
Arnav <pawar.arnav1333@gmail.com>
2025-06-24 00:16:19 +0530

    Change

--- blog_web/settings.py
+++ blog_web/settings.py
@@ -30,9 +30,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['blog-web-eez4.onrender.com', 'localhost', '127.0.0.1']
+
+ALLOWED_HOSTS = ['blog-web-eez4.onrender.com']
 
 
 # Application definition
