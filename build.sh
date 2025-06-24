b38d74962b0c10a6ff19e809a9fc5f5fe32efe28
Arnav <pawar.arnav1333@gmail.com>
2025-06-24 12:59:15 +0530

    build updated

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-# Install dependencies
+# Upgrade pip first
+pip install --upgrade pip
+
+# Install psycopg2-binary explicitly to avoid Python 3.13 incompatibility
+pip install psycopg2-binary==2.9.9
+
+# Install the rest of the requirements
 pip install -r requirements.txt
 
 # Run database migrations
