332eddf6a606ae72b540870b81c84f9e93ee8022
Arnav <pawar.arnav1333@gmail.com>
2025-06-24 12:52:06 +0530

    sqlite commented

--- blog_web/settings.py
+++ blog_web/settings.py
@@ -97,12 +97,12 @@ WSGI_APPLICATION = 'blog_web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
